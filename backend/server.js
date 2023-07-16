e897eb86c5489a2c6bd2a00fb424be0201043350
Svyatoslav Suprun <sviatoslav.suprun@nure.ua>
2023-07-16 07:46:01 +0300

    Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import ProductRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -22,6 +23,11 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products/", ProductRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
