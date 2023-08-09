adc2d206a754b24e52a5df01a7ae9e229cc816b8
Svyatoslav Suprun <sviatoslav.suprun@nure.ua>
2023-08-09 11:43:43 +0300

    products carousel

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -139,6 +139,16 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc  Get top rated products
+// @route PUT /api/products/top
+// @access Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
@@ -146,4 +156,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
